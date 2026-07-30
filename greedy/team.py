5495786080eb21db39bda34f88646d600697833c
Amrit Ray <amritray43@gmail.com>
2026-07-30 18:44:47 +0530

    Add: Codeforces 231A answer

---
+++ greedy/team.py
@@ -0,0 +1,20 @@
+class Solution():
+    def write(self, n:int) -> int:
+        implement = 0
+        
+        for _ in range(view):
+            sure = map(int, input().split())
+            row_sum = 0
+
+            for num in sure:
+                if num == 1:
+                    row_sum += 1
+
+            if row_sum > 1:
+                implement += 1
+
+        return implement
+
+view = int(input())
+
+# print(Solution().write(view))
