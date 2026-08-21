cd4a134cd7fc89d2c135ed859c825698eab3549b
Amrit Ray <amritray43@gmail.com>
2026-08-22 01:57:03 +0530

    feat: add solution for LeetCode 344 - Reverse String

--- strings/reverse_string.py
+++ strings/reverse_string.py
@@ -2,5 +2,5 @@ class Solution(object):
     def reverseString(self, s):
         s[:] = s[::-1]
 
-s = list(map(input().split(",")))
+s = list(map(input().split()))
 print(Solution().reverseString(s))
